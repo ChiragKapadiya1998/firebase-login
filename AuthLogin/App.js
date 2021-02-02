09d3d63020e8be9b18b468bee74e17096b6a5bcc
Chirag <51257430+chikskapadiya@users.noreply.github.com>
2021-02-02 11:08:32 +0530

    fix:new setup

---
+++ AuthLogin/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+       <View>
+         <Text>Login</Text>
+       </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
